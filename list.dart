1c6d1c987523341f9daa44d0a69155cc473500aa
rpierozkai29-blip <rpierozkai29@gmail.com>
2026-08-21 13:00:52 -0500

    Revert "Practica"

---
+++ list.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<String> alumnos = ["Carlos", "24", "20"];
+  
+  //Lista completa
+  print(alumnos);
+  //Dato especifico
+  print(alumnos[0]);
+  //Declarando
+  print("Alumno: ${alumnos[0]}");
+}
